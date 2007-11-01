88a06a30db1887efc829b441681cc8514b010a0f
plougher <plougher>
2007-11-01 05:35:20 +0000

    Remove obsolete CONFIG_SQUASHFS_VMALLOC configure defines

--- kernel/include/linux/squashfs_fs.h
+++ kernel/include/linux/squashfs_fs.h
@@ -28,13 +28,6 @@
 #define CONFIG_SQUASHFS_2_0_COMPATIBILITY
 #endif
 
-#ifdef	CONFIG_SQUASHFS_VMALLOC
-#define SQUASHFS_ALLOC(a)		vmalloc(a)
-#define SQUASHFS_FREE(a)		vfree(a)
-#else
-#define SQUASHFS_ALLOC(a)		kmalloc(a, GFP_KERNEL)
-#define SQUASHFS_FREE(a)		kfree(a)
-#endif
 #define SQUASHFS_CACHED_FRAGMENTS	CONFIG_SQUASHFS_FRAGMENT_CACHE_SIZE	
 #define SQUASHFS_MAJOR			3
 #define SQUASHFS_MINOR			1
