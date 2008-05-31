b15d8cb86353181971ecd3e59686cb46d720be71
plougher <plougher>
2008-05-31 15:10:36 +0000

    Update years on copyright

--- kernel/include/linux/squashfs_fs.h
+++ kernel/include/linux/squashfs_fs.h
@@ -4,7 +4,7 @@
 /*
  * Squashfs
  *
- * Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007
+ * Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007, 2008
  * Phillip Lougher <phillip@lougher.demon.co.uk>
  *
  * This program is free software; you can redistribute it and/or
