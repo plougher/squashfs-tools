656ba1eed957146009c6fc42153b6753ff44431b
plougher <plougher>
2008-08-20 05:19:26 +0000

    Update version and date.  Move version number over to release version - previously version number was the unsquashfs version (which as unsquashfs was only introduced in Squashfs 3.0 was significatly smaller than the release version).  This evidently proved to be confusing to many people :(

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -2517,7 +2517,7 @@ void progress_bar(long long current, long long max, int columns)
 
 
 #define VERSION() \
-	printf("unsquashfs version 1.6-CVS (2008/06/04)\n");\
+	printf("unsquashfs version 4.0-CVS (2008/08/20)\n");\
 	printf("copyright (C) 2008 Phillip Lougher <phillip@lougher.demon.co.uk>\n\n"); \
     	printf("This program is free software; you can redistribute it and/or\n");\
 	printf("modify it under the terms of the GNU General Public License\n");\
