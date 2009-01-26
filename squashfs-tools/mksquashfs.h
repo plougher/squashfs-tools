a708ccbed5de20252459476b94aacbb4bbcd7013
plougher <plougher>
2009-01-26 03:10:05 +0000

    New macro

--- squashfs-tools/mksquashfs.h
+++ squashfs-tools/mksquashfs.h
@@ -31,4 +31,6 @@
 #define SWAP_LE16(s, d, field)	swap_le16(&((s)->field), &((d)->field))
 #define SWAP_LE32(s, d, field)	swap_le32(&((s)->field), &((d)->field))
 #define SWAP_LE64(s, d, field)	swap_le64(&((s)->field), &((d)->field))
+#define SWAP_ULE16(s, d, field)	swap_le16((unsigned short *) &((s)->field), \
+				(unsigned short *) &((d)->field))
 #endif
