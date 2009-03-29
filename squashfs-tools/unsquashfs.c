ecd33594572f961c7e9c23cff8f4bf15acd6d319
plougher <plougher>
2009-03-29 23:21:08 +0000

    Don't bother printing failure message on TIOCGWINSZ ioctl failure if output is to a file (i.e. log file).

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -112,7 +112,8 @@ void sigwinch_handler()
 	struct winsize winsize;
 
 	if(ioctl(1, TIOCGWINSZ, &winsize) == -1) {
-		ERROR("TIOCGWINSZ ioctl failed, defaulting to 80 columns\n");
+		if(isatty(STDOUT_FILENO))
+			ERROR("TIOCGWINSZ ioctl failed, defaulting to 80 columns\n");
 		columns = 80;
 	} else
 		columns = winsize.ws_col;
