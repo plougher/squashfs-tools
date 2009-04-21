415879913fc3f9bf57a905049824f489a48813f2
plougher <plougher>
2009-04-21 00:59:16 +0000

    Fix return in read_fragment_table_4

--- squashfs-tools/unsquash-4.c
+++ squashfs-tools/unsquash-4.c
@@ -38,7 +38,7 @@ int read_fragment_table_4()
 		sBlk.fragment_table_start);
 
 	if(sBlk.fragments == 0)
-		return;
+		return TRUE;
 
 	if((fragment_table = malloc(sBlk.fragments *
 			sizeof(squashfs_fragment_entry))) == NULL)
