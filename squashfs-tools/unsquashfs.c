86673948add40cbd2fdd6214819fa4a176473829
plougher <plougher>
2010-07-21 23:31:34 +0000

    Check for realloc failure in add_subdir

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -1196,9 +1196,13 @@ struct pathnames *init_subdir()
 
 struct pathnames *add_subdir(struct pathnames *paths, struct pathname *path)
 {
-	if(paths->count % PATHS_ALLOC_SIZE == 0)
+	if(paths->count % PATHS_ALLOC_SIZE == 0) {
 		paths = realloc(paths, sizeof(struct pathnames *) +
-		(paths->count + PATHS_ALLOC_SIZE) * sizeof(struct pathname *));
+			(paths->count + PATHS_ALLOC_SIZE) *
+			sizeof(struct pathname *));
+		if(paths == NULL)
+			EXIT_UNSQUASH("Out of memory in add_subdir\n");
+	}
 
 	paths->path[paths->count++] = path;
 	return paths;
