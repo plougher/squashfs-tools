6d0e4469d1861454de828b748495386796ca837f
plougher <plougher>
2010-07-21 23:18:02 +0000

    Abort on malloc failure in add_entry, rather than return NULL which isn't checked.

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -501,22 +501,18 @@ int print_filename(char *pathname, struct inode *inode)
 }
 	
 
-int add_entry(struct hash_table_entry *hash_table[], long long start, int bytes)
+void add_entry(struct hash_table_entry *hash_table[], long long start, int bytes)
 {
 	int hash = CALCULATE_HASH(start);
 	struct hash_table_entry *hash_table_entry;
 
-	if((hash_table_entry = malloc(sizeof(struct hash_table_entry))) == NULL) {
-		ERROR("add_hash: out of memory in malloc\n");
-		return FALSE;
-	}
+	if((hash_table_entry = malloc(sizeof(struct hash_table_entry))) == NULL)
+		EXIT_UNSQUASH("Out of memory in add_entry\n");
 
 	hash_table_entry->start = start;
 	hash_table_entry->bytes = bytes;
 	hash_table_entry->next = hash_table[hash];
 	hash_table[hash] = hash_table_entry;
-
-	return TRUE;
 }
 
 
