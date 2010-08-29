2c26da18ee8d5e0e90af24c1f89d7cebb6c69fea
plougher <plougher>
2010-08-29 00:23:23 +0000

    update dates

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -1909,7 +1909,7 @@ void progress_bar(long long current, long long max, int columns)
 
 
 #define VERSION() \
-	printf("unsquashfs version 4.1-CVS (2010/08/25)\n");\
+	printf("unsquashfs version 4.1-CVS (2010/08/28)\n");\
 	printf("copyright (C) 2010 Phillip Lougher "\
 		"<phillip@lougher.demon.co.uk>\n\n");\
     	printf("This program is free software; you can redistribute it and/or"\
