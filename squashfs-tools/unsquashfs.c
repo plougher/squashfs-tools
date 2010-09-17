3bd9f0989af476290511743fce88cceb2a681d3f
plougher <plougher>
2010-09-17 03:47:36 +0000

    Hopefully these really are the release versions

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -1903,7 +1903,7 @@ void progress_bar(long long current, long long max, int columns)
 
 
 #define VERSION() \
-	printf("unsquashfs version 4.1 (2010/09/15)\n");\
+	printf("unsquashfs version 4.1 (2010/09/17)\n");\
 	printf("copyright (C) 2010 Phillip Lougher "\
 		"<phillip@lougher.demon.co.uk>\n\n");\
     	printf("This program is free software; you can redistribute it and/or"\
