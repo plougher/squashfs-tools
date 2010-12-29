4bc572a6c4fe085ab5d894d0b909fff3e0bceed6
plougher <plougher>
2010-12-29 01:45:28 +0000

    Move xz_usage() to end of file

--- squashfs-tools/xz_wrapper.c
+++ squashfs-tools/xz_wrapper.c
@@ -222,26 +222,6 @@ static void *xz_dump_options(int *size)
 }
 
 
-void xz_usage()
-{
-	fprintf(stderr, "\t  -Xbcj filter1,filter2,...,filterN\n");
-	fprintf(stderr, "\t\tCompress using filter1,filter2,...,filterN in");
-	fprintf(stderr, " turn\n\t\t(in addition to no filter), and choose");
-	fprintf(stderr, " the best compression.\n");
-	fprintf(stderr, "\t\tAvailable filters: x86, arm, armthumb,");
-	fprintf(stderr, " powerpc, sparc, ia64\n");
-	fprintf(stderr, "\t  -Xdict-size <dict-size>\n");
-	fprintf(stderr, "\t\tUse <dict-size> as the XZ dictionary size.  The");
-	fprintf(stderr, " dictionary size\n\t\tcan be specified as a");
-	fprintf(stderr, " percentage of the block size, or as an\n\t\t");
-	fprintf(stderr, "absolute value.  The dictionary size must be less");
-	fprintf(stderr, " than or equal\n\t\tto the block size and 4096 bytes");
-	fprintf(stderr, " or larger.  It must also be\n\t\tstorable in the xz");
-	fprintf(stderr, " header as either 2^n or as 2^n+2^(n+1).\n\t\t");
-	fprintf(stderr, "Example dict-sizes are 75%%, 50%%, 37.5%%, 25%%, or");
-	fprintf(stderr, " 32K, 16K, 8K\n\t\tetc.\n");
-}
-
 
 static int xz_init(void **strm, int block_size, int datablock)
 {
@@ -360,6 +340,27 @@ static int xz_uncompress(void *dest, void *src, int size, int block_size,
 
 	*error = res;
 	return res == LZMA_OK && size == (int) src_pos ? (int) dest_pos : -1;
+}
+
+
+void xz_usage()
+{
+	fprintf(stderr, "\t  -Xbcj filter1,filter2,...,filterN\n");
+	fprintf(stderr, "\t\tCompress using filter1,filter2,...,filterN in");
+	fprintf(stderr, " turn\n\t\t(in addition to no filter), and choose");
+	fprintf(stderr, " the best compression.\n");
+	fprintf(stderr, "\t\tAvailable filters: x86, arm, armthumb,");
+	fprintf(stderr, " powerpc, sparc, ia64\n");
+	fprintf(stderr, "\t  -Xdict-size <dict-size>\n");
+	fprintf(stderr, "\t\tUse <dict-size> as the XZ dictionary size.  The");
+	fprintf(stderr, " dictionary size\n\t\tcan be specified as a");
+	fprintf(stderr, " percentage of the block size, or as an\n\t\t");
+	fprintf(stderr, "absolute value.  The dictionary size must be less");
+	fprintf(stderr, " than or equal\n\t\tto the block size and 4096 bytes");
+	fprintf(stderr, " or larger.  It must also be\n\t\tstorable in the xz");
+	fprintf(stderr, " header as either 2^n or as 2^n+2^(n+1).\n\t\t");
+	fprintf(stderr, "Example dict-sizes are 75%%, 50%%, 37.5%%, 25%%, or");
+	fprintf(stderr, " 32K, 16K, 8K\n\t\tetc.\n");
 }
 
 
