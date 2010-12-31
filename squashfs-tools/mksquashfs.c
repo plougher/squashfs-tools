8691b1e847a45f88ce563e57067f3e8e4a75af6f
plougher <plougher>
2010-12-31 21:41:21 +0000

    Update date

--- squashfs-tools/mksquashfs.c
+++ squashfs-tools/mksquashfs.c
@@ -4532,7 +4532,7 @@ void read_recovery_data(char *recovery_file, char *destination_file)
 
 
 #define VERSION() \
-	printf("mksquashfs version 4.1-CVS (2010/12/30)\n");\
+	printf("mksquashfs version 4.1-CVS (2010/12/31)\n");\
 	printf("copyright (C) 2010 Phillip Lougher "\
 		"<phillip@lougher.demon.co.uk>\n\n"); \
 	printf("This program is free software; you can redistribute it and/or"\
