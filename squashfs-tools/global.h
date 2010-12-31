8f095221c8d125e3e080c6e1f5981e761774c74d
plougher <plougher>
2010-12-31 09:51:28 +0000

    Get rid of typdef of union squashfs_inode_header

--- squashfs-tools/global.h
+++ squashfs-tools/global.h
@@ -39,7 +39,6 @@ typedef struct squashfs_ldir_inode_header squashfs_ldir_inode_header;
 typedef struct squashfs_dir_entry squashfs_dir_entry;
 typedef struct squashfs_dir_header squashfs_dir_header;
 typedef struct squashfs_fragment_entry squashfs_fragment_entry;
-typedef union squashfs_inode_header squashfs_inode_header;
 
 typedef unsigned int squashfs_id;
 typedef long long squashfs_fragment_index;
