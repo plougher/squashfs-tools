3ec445d6abf11dad3f86d346ebc09dcddbc5354d
Dave Vasilevsky <dave@vasilevsky.ca>
2012-03-19 05:49:36 -0400

    Mac, BSD: FNM_EXTMATCH unavailable
    The flag FNM_EXTMATCH for fnmatch() is a glibc extension. Make it a no-op when it's unavailable.

--- squashfs-tools/action.c
+++ squashfs-tools/action.c
@@ -39,6 +39,10 @@
 #include <limits.h>
 #include <errno.h>
 
+#ifndef FNM_EXTMATCH /* glibc extension */
+    #define FNM_EXTMATCH 0
+#endif
+
 #include "squashfs_fs.h"
 #include "mksquashfs.h"
 #include "action.h"
