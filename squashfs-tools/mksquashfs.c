46e77ac453677cb3be175752c34886bfeeb09878
Phillip Lougher <phillip@squashfs.org.uk>
2012-10-18 23:55:37 +0100

    mksquashfs: Split progress bar out into separate files
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/mksquashfs.c
+++ squashfs-tools/mksquashfs.c
@@ -32,7 +32,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stddef.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -42,11 +41,9 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <setjmp.h>
-#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <pthread.h>
-#include <math.h>
 #include <regex.h>
 #include <fnmatch.h>
 #include <sys/wait.h>
@@ -78,11 +75,7 @@
 
 #define ERROR(s, args...) \
 		do {\
-			pthread_mutex_lock(&progress_mutex); \
-			if(progress_enabled) \
-				fprintf(stderr, "\n"); \
-			fprintf(stderr, s, ## args);\
-			pthread_mutex_unlock(&progress_mutex); \
+			progressbar_error(s, ## args); \
 		} while(0)
 
 #define EXIT_MKSQUASHFS() \
@@ -96,11 +89,7 @@
 
 #define BAD_ERROR(s, args...) \
 		do {\
-			pthread_mutex_lock(&progress_mutex); \
-			if(progress_enabled) \
-				fprintf(stderr, "\n"); \
-			fprintf(stderr, "FATAL ERROR:" s, ##args);\
-			pthread_mutex_unlock(&progress_mutex); \
+			progressbar_error("FATAL ERROR:" s, ##args); \
 			EXIT_MKSQUASHFS();\
 		} while(0)
 
@@ -112,11 +101,10 @@
 #include "compressor.h"
 #include "xattr.h"
 #include "action.h"
+#include "progressbar.h"
 
 int delete = FALSE;
 int fd;
-int cur_uncompressed = 0, estimated_uncompressed = 0;
-int columns;
 
 /* filesystem flags for building */
 int comp_opts = FALSE;
@@ -127,7 +115,6 @@ int silent = TRUE;
 long long global_uid = -1, global_gid = -1;
 int exportable = TRUE;
 int progress = TRUE;
-int progress_enabled = FALSE;
 int sparse_files = TRUE;
 int old_exclude = TRUE;
 int use_regex = FALSE;
@@ -360,13 +347,10 @@ unsigned int sid_count = 0, suid_count = 0, sguid_count = 0;
 struct cache *reader_buffer, *writer_buffer, *fragment_buffer;
 struct queue *to_reader, *from_reader, *to_writer, *from_writer, *from_deflate,
 	*to_frag;
-pthread_t *thread, *deflator_thread, *frag_deflator_thread, progress_thread;
+pthread_t *thread, *deflator_thread, *frag_deflator_thread;
 pthread_mutex_t	fragment_mutex;
 pthread_cond_t fragment_waiting;
 pthread_mutex_t	pos_mutex;
-pthread_mutex_t progress_mutex;
-pthread_cond_t progress_wait;
-int rotate = 0;
 struct pseudo *pseudo = NULL;
 
 /* user options that control parallelisation */
@@ -425,7 +409,6 @@ struct file_info *add_non_dup(long long file_size, long long bytes,
 extern int generate_file_priorities(struct dir_info *dir, int priority,
 	struct stat *buf);
 extern struct priority_entry *priority_list[65536];
-void progress_bar(long long current, long long max, int columns);
 long long generic_write_table(int, void *, int, void *, int);
 void restorefs();
 struct dir_info *scan1_opendir(char *pathname, char *subpath, int depth);
@@ -722,20 +705,6 @@ void cache_block_put(struct file_buffer *entry)
 			+ (((char *)A) - data_cache)))
 
 
-inline void inc_progress_bar()
-{
-	cur_uncompressed ++;
-}
-
-
-inline void update_progress_bar()
-{
-	pthread_mutex_lock(&progress_mutex);
-	pthread_cond_signal(&progress_wait);
-	pthread_mutex_unlock(&progress_mutex);
-}
-
-
 inline void waitforthread(int i)
 {
 	TRACE("Waiting for thread %d\n", i);
@@ -835,26 +804,6 @@ void sigusr1_handler()
 		sigsuspend(&sigmask);
 		TRACE("After wait in sigusr1_handler :(\n");
 	}
-}
-
-
-void sigwinch_handler()
-{
-	struct winsize winsize;
-
-	if(ioctl(1, TIOCGWINSZ, &winsize) == -1) {
-		if(isatty(STDOUT_FILENO))
-			printf("TIOCGWINSZ ioctl failed, defaulting to 80 "
-				"columns\n");
-		columns = 80;
-	} else
-		columns = winsize.ws_col;
-}
-
-
-void sigalrm_handler()
-{
-	rotate = (rotate + 1) % 4;
 }
 
 
@@ -2363,13 +2312,13 @@ void reader_read_process(struct dir_ent *dir_ent)
 			break;
 
 		/*
-		 * Update estimated_uncompressed block count.  This is done
+		 * Update progress bar size.  This is done
 		 * on every block rather than waiting for all blocks to be
 		 * read incase write_file_process() is running in parallel
-		 * with this.  Otherwise cur uncompressed block count may
-		 * get ahead of the total uncompressed block count.
+		 * with this.  Otherwise the current progress bar position
+		 * may get ahead of the progress bar size.
 		 */ 
-		estimated_uncompressed ++;
+		progress_bar_size(1);
 
 		if(prev_buffer)
 			queue_put(from_reader, prev_buffer);
@@ -2761,113 +2710,6 @@ struct file_buffer *get_file_buffer(struct queue *queue)
 }
 
 
-void *progress_thrd(void *arg)
-{
-	struct timeval timeval;
-	struct timespec timespec;
-	struct itimerval itimerval;
-	struct winsize winsize;
-
-	if(ioctl(1, TIOCGWINSZ, &winsize) == -1) {
-		if(isatty(STDOUT_FILENO))
-			printf("TIOCGWINSZ ioctl failed, defaulting to 80 "
-				"columns\n");
-		columns = 80;
-	} else
-		columns = winsize.ws_col;
-	signal(SIGWINCH, sigwinch_handler);
-	signal(SIGALRM, sigalrm_handler);
-
-	itimerval.it_value.tv_sec = 0;
-	itimerval.it_value.tv_usec = 250000;
-	itimerval.it_interval.tv_sec = 0;
-	itimerval.it_interval.tv_usec = 250000;
-	setitimer(ITIMER_REAL, &itimerval, NULL);
-
-	pthread_cond_init(&progress_wait, NULL);
-
-	pthread_mutex_lock(&progress_mutex);
-
-	while(1) {
-		gettimeofday(&timeval, NULL);
-		timespec.tv_sec = timeval.tv_sec;
-		if(timeval.tv_usec + 250000 > 999999)
-			timespec.tv_sec++;
-		timespec.tv_nsec = ((timeval.tv_usec + 250000) % 1000000) *
-			1000;
-		pthread_cond_timedwait(&progress_wait, &progress_mutex,
-			&timespec);
-		if(progress_enabled && estimated_uncompressed)
-			progress_bar(cur_uncompressed, estimated_uncompressed,
-				columns);
-	}
-}
-
-
-void enable_progress_bar()
-{
-	pthread_mutex_lock(&progress_mutex);
-	progress_enabled = TRUE;
-	pthread_mutex_unlock(&progress_mutex);
-}
-
-
-void disable_progress_bar()
-{
-	pthread_mutex_lock(&progress_mutex);
-	progress_enabled = FALSE;
-	pthread_mutex_unlock(&progress_mutex);
-}
-
-
-void progress_bar(long long current, long long max, int columns)
-{
-	char rotate_list[] = { '|', '/', '-', '\\' };
-	int max_digits, used, hashes, spaces;
-	static int tty = -1;
-
-	if(max == 0)
-		return;
-
-	max_digits = floor(log10(max)) + 1;
-	used = max_digits * 2 + 11;
-	hashes = (current * (columns - used)) / max;
-	spaces = columns - used - hashes;
-
-	if((current > max) || (columns - used < 0))
-		return;
-
-	if(tty == -1)
-		tty = isatty(STDOUT_FILENO);
-	if(!tty) {
-		static long long previous = -1;
-
-		/* Updating much more frequently than this results in huge
-		 * log files. */
-		if((current % 100) != 0 && current != max)
-			return;
-		/* Don't update just to rotate the spinner. */
-		if(current == previous)
-			return;
-		previous = current;
-	}
-
-	printf("\r[");
-
-	while (hashes --)
-		putchar('=');
-
-	putchar(rotate_list[rotate]);
-
-	while(spaces --)
-		putchar(' ');
-
-	printf("] %*lld/%*lld", max_digits, current, max_digits, max);
-	printf(" %3lld%%", current * 100 / max);
-	fflush(stdout);
-}
-
-
 void write_file_empty(squashfs_inode *inode, struct dir_ent *dir_ent,
 	int *duplicate_file)
 {
@@ -3011,7 +2853,7 @@ int write_file_process(squashfs_inode *inode, struct dir_ent *dir_ent,
 	return 0;
 
 read_err:
-	cur_uncompressed -= block;
+	dec_progress_bar(block);
 	status = read_buffer->error;
 	bytes = start;
 	if(!block_device) {
@@ -3110,7 +2952,7 @@ int write_file_blocks(squashfs_inode *inode, struct dir_ent *dir_ent,
 	return 0;
 
 read_err:
-	cur_uncompressed -= block;
+	dec_progress_bar(block);
 	status = read_buffer->error;
 	bytes = start;
 	if(!block_device) {
@@ -3248,7 +3090,7 @@ int write_file_blocks_dup(squashfs_inode *inode, struct dir_ent *dir_ent,
 	return 0;
 
 read_err:
-	cur_uncompressed -= block;
+	dec_progress_bar(block);
 	status = read_buffer->error;
 	bytes = start;
 	if(thresh && !block_device) {
@@ -3418,8 +3260,7 @@ struct inode_info *lookup_inode2(struct stat *buf, int pseudo, int id)
 	inode->noF = noF;
 
 	if((buf->st_mode & S_IFMT) == S_IFREG)
-		estimated_uncompressed += (buf->st_size + block_size - 1) >>
-			block_log;
+		progress_bar_size((buf->st_size + block_size - 1) >> block_log);
 
 	inode->next = inode_info[ino_hash];
 	inode_info[ino_hash] = inode;
@@ -4411,7 +4252,7 @@ void initialise_threads(int readb_mbytes, int writeb_mbytes,
 	fragment_buffer = cache_init(block_size, fragment_buffer_size);
 	pthread_create(&thread[0], NULL, reader, NULL);
 	pthread_create(&thread[1], NULL, writer, NULL);
-	pthread_create(&progress_thread, NULL, progress_thrd, NULL);
+	init_progress_bar();
 	pthread_mutex_init(&fragment_mutex, NULL);
 	pthread_cond_init(&fragment_waiting, NULL);
 
@@ -4840,7 +4681,7 @@ void read_recovery_data(char *recovery_file, char *destination_file)
 
 
 #define VERSION() \
-	printf("mksquashfs version 4.2-CVS (2012/10/13)\n");\
+	printf("mksquashfs version 4.2-CVS (2012/10/18)\n");\
 	printf("copyright (C) 2012 Phillip Lougher "\
 		"<phillip@lougher.demon.co.uk>\n\n"); \
 	printf("This program is free software; you can redistribute it and/or"\
@@ -4869,7 +4710,6 @@ int main(int argc, char *argv[])
 		writeb_mbytes = WRITER_BUFFER_DEFAULT,
 		fragmentb_mbytes = FRAGMENT_BUFFER_DEFAULT;
 
-	pthread_mutex_init(&progress_mutex, NULL);
 	block_log = slog(block_size);
 	if(argc > 1 && strcmp(argv[1], "-version") == 0) {
 		VERSION();
@@ -5615,10 +5455,8 @@ printOptions:
 	sBlk.mkfs_time = time(NULL);
 
 restore_filesystem:
-	if(progress && estimated_uncompressed) {
+	if(progress)
 		disable_progress_bar();
-		progress_bar(cur_uncompressed, estimated_uncompressed, columns);
-	}
 
 	sBlk.fragments = fragments;
 	if(!restoring) {
---
+++ squashfs-tools/progressbar.c
@@ -0,0 +1,218 @@
+/*
+ * Create a squashfs filesystem.  This is a highly compressed read only
+ * filesystem.
+ *
+ * Copyright (c) 2012
+ * Phillip Lougher <phillip@lougher.demon.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * progressbar.c
+ */
+
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdarg.h>
+
+int progress_enabled = 0;
+int rotate = 0;
+int cur_uncompressed = 0, estimated_uncompressed = 0;
+int columns;
+
+pthread_t progress_thread;
+pthread_mutex_t progress_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t progress_wait;
+
+
+static void sigwinch_handler()
+{
+	struct winsize winsize;
+
+	if(ioctl(1, TIOCGWINSZ, &winsize) == -1) {
+		if(isatty(STDOUT_FILENO))
+			printf("TIOCGWINSZ ioctl failed, defaulting to 80 "
+				"columns\n");
+		columns = 80;
+	} else
+		columns = winsize.ws_col;
+}
+
+
+static void sigalrm_handler()
+{
+	rotate = (rotate + 1) % 4;
+}
+
+
+void inc_progress_bar()
+{
+	cur_uncompressed ++;
+}
+
+
+void dec_progress_bar(int count)
+{
+	cur_uncompressed -= count;
+}
+
+
+void progress_bar_size(int count)
+{
+	estimated_uncompressed += count;
+}
+
+
+void update_progress_bar()
+{
+	pthread_mutex_lock(&progress_mutex);
+	pthread_cond_signal(&progress_wait);
+	pthread_mutex_unlock(&progress_mutex);
+}
+
+
+static void progress_bar(long long current, long long max, int columns)
+{
+	char rotate_list[] = { '|', '/', '-', '\\' };
+	int max_digits, used, hashes, spaces;
+	static int tty = -1;
+
+	if(max == 0)
+		return;
+
+	max_digits = floor(log10(max)) + 1;
+	used = max_digits * 2 + 11;
+	hashes = (current * (columns - used)) / max;
+	spaces = columns - used - hashes;
+
+	if((current > max) || (columns - used < 0))
+		return;
+
+	if(tty == -1)
+		tty = isatty(STDOUT_FILENO);
+	if(!tty) {
+		static long long previous = -1;
+
+		/* Updating much more frequently than this results in huge
+		 * log files. */
+		if((current % 100) != 0 && current != max)
+			return;
+		/* Don't update just to rotate the spinner. */
+		if(current == previous)
+			return;
+		previous = current;
+	}
+
+	printf("\r[");
+
+	while (hashes --)
+		putchar('=');
+
+	putchar(rotate_list[rotate]);
+
+	while(spaces --)
+		putchar(' ');
+
+	printf("] %*lld/%*lld", max_digits, current, max_digits, max);
+	printf(" %3lld%%", current * 100 / max);
+	fflush(stdout);
+}
+
+
+void enable_progress_bar()
+{
+	pthread_mutex_lock(&progress_mutex);
+	progress_enabled = 1;
+	pthread_mutex_unlock(&progress_mutex);
+}
+
+
+void disable_progress_bar()
+{
+	pthread_mutex_lock(&progress_mutex);
+	progress_enabled = 0;
+	progress_bar(cur_uncompressed, estimated_uncompressed, columns);
+	pthread_mutex_unlock(&progress_mutex);
+}
+
+
+void *progress_thrd(void *arg)
+{
+	struct timeval timeval;
+	struct timespec timespec;
+	struct itimerval itimerval;
+	struct winsize winsize;
+
+	if(ioctl(1, TIOCGWINSZ, &winsize) == -1) {
+		if(isatty(STDOUT_FILENO))
+			printf("TIOCGWINSZ ioctl failed, defaulting to 80 "
+				"columns\n");
+		columns = 80;
+	} else
+		columns = winsize.ws_col;
+	signal(SIGWINCH, sigwinch_handler);
+	signal(SIGALRM, sigalrm_handler);
+
+	itimerval.it_value.tv_sec = 0;
+	itimerval.it_value.tv_usec = 250000;
+	itimerval.it_interval.tv_sec = 0;
+	itimerval.it_interval.tv_usec = 250000;
+	setitimer(ITIMER_REAL, &itimerval, NULL);
+
+	pthread_mutex_lock(&progress_mutex);
+
+	while(1) {
+		gettimeofday(&timeval, NULL);
+		timespec.tv_sec = timeval.tv_sec;
+		if(timeval.tv_usec + 250000 > 999999)
+			timespec.tv_sec++;
+		timespec.tv_nsec = ((timeval.tv_usec + 250000) % 1000000) *
+			1000;
+		pthread_cond_timedwait(&progress_wait, &progress_mutex,
+			&timespec);
+		if(progress_enabled && estimated_uncompressed)
+			progress_bar(cur_uncompressed, estimated_uncompressed,
+				columns);
+	}
+}
+
+
+void init_progress_bar()
+{
+	pthread_cond_init(&progress_wait, NULL);
+	pthread_create(&progress_thread, NULL, progress_thrd, NULL);
+}
+
+
+void progressbar_error(char *fmt, ...)
+{
+	va_list ap;
+
+	pthread_mutex_lock(&progress_mutex);
+
+	if(progress_enabled)
+		fprintf(stderr, "\n");
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	pthread_mutex_unlock(&progress_mutex);
+}
+
