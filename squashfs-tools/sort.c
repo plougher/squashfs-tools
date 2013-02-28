c3a414f7d8a6460ccee7bdfa942f5e48b70e8711
Phillip Lougher <phillip@squashfs.org.uk>
2013-02-28 23:56:39 +0000

    Update copyright
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/sort.c
+++ squashfs-tools/sort.c
@@ -2,7 +2,8 @@
  * Create a squashfs filesystem.  This is a highly compressed read only
  * filesystem.
  *
- * Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012
+ * Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012,
+ * 2013
  * Phillip Lougher <phillip@squashfs.org.uk>
  *
  * This program is free software; you can redistribute it and/or
