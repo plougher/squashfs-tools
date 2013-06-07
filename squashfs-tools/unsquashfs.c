f122bb0a5a16669445667a998b65633f64962695
Phillip Lougher <phillip@squashfs.org.uk>
2013-06-07 04:04:06 +0100

    unsquashfs: add dump_queue()
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -224,6 +224,21 @@ void *queue_get(struct queue *queue)
 }
 
 
+void dump_queue(struct queue *queue)
+{
+	pthread_mutex_lock(&queue->mutex);
+
+	printf("Max size %d, size %d%s\n", queue->size - 1,  
+		queue->readp <= queue->writep ? queue->writep - queue->readp :
+			queue->size - queue->readp + queue->writep,
+		queue->readp == queue->writep ? " (EMPTY)" :
+			((queue->writep + 1) % queue->size) == queue->readp ?
+			" (FULL)" : "");
+
+	pthread_mutex_unlock(&queue->mutex);
+}
+
+
 /* Called with the cache mutex held */
 void insert_hash_table(struct cache *cache, struct cache_entry *entry)
 {
