488fc22f9a1e07efc89dfe3836b744fab31d5793
Phillip Lougher <phillip@squashfs.org.uk>
2013-06-17 04:36:00 +0100

    lz4: add experimental support for lz4 compression
    * IMPORTANT HIGHLY EXPERIMENTAL * only use for experimental
    testing of lz4 compression.  LZ4 compressed filesystems produced
    by this code will be guaranteed to become unreadable in a couple of
    days!
    
    This code produces "legacy" LZ4 format compression - this is the
    format supported by the LZ4 kernel code currently in linux-next.
    
    It is currently unclear whether the kernel code expects the LZ4
    magic to be present - if so this has to be explictly added as it is not
    generated by the LZ4 calls.
    
    More importantly, it is unclear whether in the long term the
    kernel code will continue to use the "legacy" format or move to the
    more complicated newer format.
    
    Due to this it is important to mark these LZ4 compressed streams as
    using "legacy" format, to enable the code to distinquish between
    "legacy" format and the newer format, in the event the kernel code
    moves to the newer format.
    
    This could be achieved by having two compression types, LZ4 == legacy, and
    LZ4_XXX for any newer format, but this is messy, and makes an
    unnecessary distinction (at the level of the compression type) between
    the formats.
    
    A better option is to make comp_opts (compression options) mandatory
    for LZ4, and this comp_opt structure will encode the version of the
    format, which will be used in the event the legacy format is superseded
    in the kernel.
    
    Currently this code lacks setting a comp_opts structure.  In the
    near future this code will be added, and the presence of the
    comp_opts structure in the filesystem will become mandatory - and
    any filesystems generated by this code will become unreadable.  You have
    been warned!
    
    Checking into lz4 branch for this reason!
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

---
+++ squashfs-tools/lz4_wrapper.c
@@ -0,0 +1,235 @@
+/*
+ * Copyright (c) 2013
+ * Phillip Lougher <phillip@squashfs.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * lz4_wrapper.c
+ *
+ * Support for LZ4 compression http://xxx
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <lz4.h>
+
+#include "squashfs_fs.h"
+#include "lz4_wrapper.h"
+#include "compressor.h"
+
+static struct lz4_comp_opts comp_opts;
+
+/*
+ * This function is called by the options parsing code in mksquashfs.c
+ * to parse any -X compressor option.
+ *
+ * This function returns 1 on successful parsing of an option
+ *			-1 if the option was unrecognised, or
+ *			-2 if the option was recognised, but otherwise bad in
+ *			   some way (e.g. invalid parameter) 
+ *
+ * Note: this function sets internal compressor state, but does not
+ * pass back the results of the parsing other than success/failure.
+ * The lz4_dump_options() function is called later to get the options in
+ * a format suitable for writing to the filesystem.
+ */
+static int lz4_options(char *argv[], int argc)
+{
+	return -1;
+	
+failed:
+	return -2;
+}
+
+
+/*
+ * This function is called after all options have been parsed.
+ * It is used to do post-processing on the compressor options using
+ * values that were not expected to be known at option parse time.
+ *
+ * XXX needed?
+ *
+ * This function returns 0 on successful post processing, or
+ *			-1 on error
+ */
+static int lz4_options_post(int block_size)
+{
+	return 0;
+
+failed:
+	return -1;
+}
+
+
+/*
+ * This function is called by mksquashfs to dump the parsed
+ * compressor options in a format suitable for writing to the
+ * compressor options field in the filesystem (stored immediately
+ * after the superblock).
+ *
+ * This function returns a pointer to the compression options structure
+ * to be stored (and the size), or NULL if there are no compression
+ * options
+ */
+static void *lz4_dump_options(int block_size, int *size)
+{
+#if 0
+	//SQUASHFS_INSWAP_COMP_OPTS(&comp_opts);
+
+	*size = sizeof(comp_opts);
+	return &comp_opts;
+#endif
+
+	return NULL;
+}
+
+
+/*
+ * This function is a helper specifically for the append mode of
+ * mksquashfs.  Its purpose is to set the internal compressor state
+ * to the stored compressor options in the passed compressor options
+ * structure.
+ *
+ * In effect this function sets up the compressor options
+ * to the same state they were when the filesystem was originally
+ * generated, this is to ensure on appending, the compressor uses
+ * the same compression options that were used to generate the
+ * original filesystem.
+ *
+ * Note, even if there are no compressor options, this function is still
+ * called with an empty compressor structure (size == 0), to explicitly
+ * set the default options, this is to ensure any user supplied
+ * -X options on the appending mksquashfs command line are over-ridden
+ *
+ * This function returns 0 on sucessful extraction of options, and
+ *			-1 on error
+ */
+static int lz4_extract_options(int block_size, void *buffer, int size)
+{
+	struct comp_opts *comp_opts = buffer;
+
+	if(size == 0) {
+		/* set defaults */
+	} else {
+	}
+
+	return 0;
+
+failed:
+	fprintf(stderr, "lz4: error reading stored compressor options from "
+		"filesystem!\n");
+
+	return -1;
+}
+
+
+void lz4_display_options(void *buffer, int size)
+{
+	struct comp_opts *comp_opts = buffer;
+
+	/* check passed comp opts struct is of the correct length */
+	if(size != sizeof(comp_opts))
+		goto failed;
+
+	//SQUASHFS_INSWAP_COMP_OPTS(comp_opts);
+
+	return;
+
+failed:
+	fprintf(stderr, "lz4: error reading stored compressor options from "
+		"filesystem!\n");
+}	
+
+
+/*
+ * This function is called by mksquashfs to initialise the
+ * compressor, before compress() is called.
+ *
+ * This function returns 0 on success, and
+ *			-1 on error
+ */
+static int lz4_init(void **strm, int block_size, int datablock)
+{
+#if 0
+	struct lz4_stream *stream;
+
+	stream = *strm = malloc(sizeof(struct xz_stream));
+	if(stream == NULL)
+		goto failed;
+#endif
+
+	return 0;
+
+failed:
+	return -1;
+}
+
+
+static int lz4_compress(void *strm, void *dest, void *src,  int size,
+	int block_size, int *error)
+{
+	//struct lz4_stream *stream = strm;
+	int res = LZ4_compress_limitedOutput(src, dest, size, block_size);
+	if(res == 0) {
+		/*
+	 	 * Output buffer overflow.  Return out of buffer space
+	 	 */
+		return 0;
+	} else if(res < 0) {
+		/*
+	 	 * All other errors return failure, with the compressor
+	 	 * specific error code in *error
+	 	 */
+		*error = res;
+		return -1;
+	}
+
+	return res;
+}
+
+
+static int lz4_uncompress(void *dest, void *src, int size, int outsize,
+	int *error)
+{
+	int res = LZ4_decompress_safe(src, dest, size, outsize);
+	if(res < 0) {
+		*error = res;
+		return -1;
+	}
+
+	return res;
+}
+
+
+void lz4_usage()
+{
+}
+
+
+struct compressor lz4_comp_ops = {
+	.init = lz4_init,
+	.compress = lz4_compress,
+	.uncompress = lz4_uncompress,
+	.options = lz4_options,
+	.options_post = lz4_options_post,
+	.dump_options = lz4_dump_options,
+	.extract_options = lz4_extract_options,
+	.display_options = lz4_display_options,
+	.usage = lz4_usage,
+	.id = LZ4_COMPRESSION,
+	.name = "lz4",
+	.supported = 1
+};
---
+++ squashfs-tools/lz4_wrapper.h
@@ -0,0 +1,48 @@
+#ifndef LZ4_WRAPPER_H
+#define LZ4_WRAPPER_H
+/*
+ * Squashfs
+ *
+ * Copyright (c) 2013
+ * Phillip Lougher <phillip@squashfs.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * lz4_wrapper.h
+ *
+ */
+
+#ifndef linux
+#define __BYTE_ORDER BYTE_ORDER
+#define __BIG_ENDIAN BIG_ENDIAN
+#define __LITTLE_ENDIAN LITTLE_ENDIAN
+#else
+#include <endian.h>
+#endif
+
+#if __BYTE_ORDER == __BIG_ENDIAN
+extern unsigned int inswap_le32(unsigned int);
+
+#define SQUASHFS_INSWAP_COMP_OPTS(s) { \
+	(s)->dictionary_size = inswap_le32((s)->dictionary_size); \
+	(s)->flags = inswap_le32((s)->flags); \
+}
+#else
+#define SQUASHFS_INSWAP_COMP_OPTS(s)
+#endif
+
+struct lz4_comp_opts {
+};
+#endif
