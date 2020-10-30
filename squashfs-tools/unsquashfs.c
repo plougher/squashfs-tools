e595864642cd6fdaa22ef24a81b8fc26ffc00845
Phillip Lougher <phillip@squashfs.org.uk>
2020-10-30 12:53:59 +0000

    Unsquashfs: fix case where malloc(0) returns NULL
    Malloc(0) may return NULL, which would be treated as an error.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -995,7 +995,7 @@ int write_file(struct inode *inode, char *pathname)
 	}
 
 	block_list = malloc(inode->blocks * sizeof(unsigned int));
-	if(block_list == NULL)
+	if(block_list == NULL && inode->blocks)
 		EXIT_UNSQUASH("write_file: unable to malloc block list\n");
 
 	s_ops->read_block_list(block_list, inode->block_ptr, inode->blocks);
