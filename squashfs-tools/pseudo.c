7963f9a76dbeb57ffa28b11f03efc7f7fe5a3f07
Phillip Lougher <phillip@squashfs.org.uk>
2021-04-06 19:16:04 +0100

    Pseudo: Merge read_file() into read_pseudo_file()
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/pseudo.c
+++ squashfs-tools/pseudo.c
@@ -49,120 +49,6 @@ struct pseudo_dev **pseudo_file = NULL;
 struct pseudo *pseudo = NULL;
 int pseudo_count = 0;
 
-/*
- * Read file, passing each line to parse_line() for
- * parsing.
- *
- * Lines can be split across multiple lines using "\".
- * 
- * Blank lines and comment lines indicated by # are supported.
- */
-static int read_file(char *filename, char *destination, int (parse_line)(char *, char *))
-{
-	FILE *fd;
-	char *def, *err, *line = NULL;
-	int res, size = 0;
-
-	fd = fopen(filename, "r");
-	if(fd == NULL) {
-		ERROR("Could not open pseudo device file \"%s\" because %s\n",
-			filename, strerror(errno));
-		return FALSE;
-	}
-
-	while(1) {
-		int total = 0;
-
-		while(1) {
-			int len;
-
-			if(total + (MAX_LINE + 1) > size) {
-				line = realloc(line, size += (MAX_LINE + 1));
-				if(line == NULL)
-					MEM_ERROR();
-			}
-
-			err = fgets(line + total, MAX_LINE + 1, fd);
-			if(err == NULL)
-				break;
-
-			len = strlen(line + total);
-			total += len;
-
-			if(len == MAX_LINE && line[total - 1] != '\n') {
-				/* line too large */
-				ERROR("Line too long when reading "
-					"pseudo file \"%s\", larger than "
-					"%d bytes\n", filename, MAX_LINE);
-				goto failed;
-			}
-
-			/*
-			 * Remove '\n' terminator if it exists (the last line
-			 * in the file may not be '\n' terminated)
-			 */
-			if(len && line[total - 1] == '\n') {
-				line[-- total] = '\0';
-				len --;
-			}
-
-			/*
-			 * If no line continuation then jump out to
-			 * process line.  Note, we have to be careful to
-			 * check for "\\" (backslashed backslash) and to
-			 * ensure we don't look at the previous line
-			 */
-			if(len == 0 || line[total - 1] != '\\' || (len >= 2 &&
-					strcmp(line + total - 2, "\\\\") == 0))
-				break;
-			else
-				total --;
-		}	
-
-		if(err == NULL) {
-			if(ferror(fd)) {
-				ERROR("Reading pseudo file \"%s\" failed "
-					"because %s\n", filename,
-					strerror(errno));
-				goto failed;
-			}
-
-			/*
-			 * At EOF, normally we'll be finished, but, have to
-			 * check for special case where we had "\" line
-			 * continuation and then hit EOF immediately afterwards
-			 */
-			if(total == 0)
-				break;
-			else
-				line[total] = '\0';
-		}
-
-		/* Skip any leading whitespace */
-		for(def = line; isspace(*def); def ++);
-
-		/* if line is now empty after skipping characters, skip it */
-		if(*def == '\0')
-			continue;
-
-		/* if comment line, skip */
-		if(*def == '#')
-			continue;
-
-		res = parse_line(def, destination);
-		if(res == FALSE)
-			goto failed;
-	}
-
-	fclose(fd);
-	free(line);
-	return TRUE;
-
-failed:
-	fclose(fd);
-	free(line);
-	return FALSE;
-}
 static char *get_component(char *target, char **targname)
 {
 	char *start;
@@ -1304,7 +1190,109 @@ int read_pseudo_definition(char *filename, char *destination)
 
 int read_pseudo_file(char *filename, char *destination)
 {
-	return read_file(filename, destination, read_pseudo_def);
+	FILE *fd;
+	char *def, *err, *line = NULL;
+	int res, size = 0;
+
+	fd = fopen(filename, "r");
+	if(fd == NULL) {
+		ERROR("Could not open pseudo device file \"%s\" because %s\n",
+			filename, strerror(errno));
+		return FALSE;
+	}
+
+	while(1) {
+		int total = 0;
+
+		while(1) {
+			int len;
+
+			if(total + (MAX_LINE + 1) > size) {
+				line = realloc(line, size += (MAX_LINE + 1));
+				if(line == NULL)
+					MEM_ERROR();
+			}
+
+			err = fgets(line + total, MAX_LINE + 1, fd);
+			if(err == NULL)
+				break;
+
+			len = strlen(line + total);
+			total += len;
+
+			if(len == MAX_LINE && line[total - 1] != '\n') {
+				/* line too large */
+				ERROR("Line too long when reading "
+					"pseudo file \"%s\", larger than "
+					"%d bytes\n", filename, MAX_LINE);
+				goto failed;
+			}
+
+			/*
+			 * Remove '\n' terminator if it exists (the last line
+			 * in the file may not be '\n' terminated)
+			 */
+			if(len && line[total - 1] == '\n') {
+				line[-- total] = '\0';
+				len --;
+			}
+
+			/*
+			 * If no line continuation then jump out to
+			 * process line.  Note, we have to be careful to
+			 * check for "\\" (backslashed backslash) and to
+			 * ensure we don't look at the previous line
+			 */
+			if(len == 0 || line[total - 1] != '\\' || (len >= 2 &&
+					strcmp(line + total - 2, "\\\\") == 0))
+				break;
+			else
+				total --;
+		}
+
+		if(err == NULL) {
+			if(ferror(fd)) {
+				ERROR("Reading pseudo file \"%s\" failed "
+					"because %s\n", filename,
+					strerror(errno));
+				goto failed;
+			}
+
+			/*
+			 * At EOF, normally we'll be finished, but, have to
+			 * check for special case where we had "\" line
+			 * continuation and then hit EOF immediately afterwards
+			 */
+			if(total == 0)
+				break;
+			else
+				line[total] = '\0';
+		}
+
+		/* Skip any leading whitespace */
+		for(def = line; isspace(*def); def ++);
+
+		/* if line is now empty after skipping characters, skip it */
+		if(*def == '\0')
+			continue;
+
+		/* if comment line, skip */
+		if(*def == '#')
+			continue;
+
+		res = read_pseudo_def(def, destination);
+		if(res == FALSE)
+			goto failed;
+	}
+
+	fclose(fd);
+	free(line);
+	return TRUE;
+
+failed:
+	fclose(fd);
+	free(line);
+	return FALSE;
 }
 
 
