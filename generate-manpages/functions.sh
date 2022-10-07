c68e0b13109b4c9418f61a75edbf7b89fa3827b1
Phillip Lougher <phillip@squashfs.org.uk>
2022-10-07 21:12:23 +0100

    functions.sh: fix echo output to stderr
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- generate-manpages/functions.sh
+++ generate-manpages/functions.sh
@@ -2,8 +2,8 @@
 
 # Sanity check, check that the non-builtin echo exists and is in PATH
 if ! which echo > /dev/null 2>&1; then
-	echo "$0: This script needs the non-builtin echo, which is not in your PATH." 2>&1
-	echo "$0: Fix PATH or install before running this script!" 2>&1
+	echo "$0: This script needs the non-builtin echo, which is not in your PATH." >&2
+	echo "$0: Fix PATH or install before running this script!" >&2
 	exit 1
 fi
 
