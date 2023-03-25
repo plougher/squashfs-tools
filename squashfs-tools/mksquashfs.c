plougher/squashfs-tools

commit 94db791f11cbe6512e2e4dde55d6b625fec21d05

Mksquashfs: fix rare race condition in fragment writing/duplicate check
This race condition only occurs using very small caches, produced
by setting the -mem option to a very low value, for example 20 Mbytes.

When the compressed fragment block is written to the fragment orderer
thread, the fragment_table isn't updated until it is processed by
the fragment orderer thread.  Which leaves the fragment table
not updated for the time it is in the queue and in transit to the
fragment orderer thread.

There are some front-end fragment de-duplication threads which
de-duplicate fragments (if possible) before they are processed
and de-duplicated by the main thread.  If a fragment is a possible
duplicate of a fragment contained in a previously created fragment
block, they first try to lookup the uncompressed fragment block in the
fragment block cache (which holds the last X created fragment blocks
on a freelist, before reuse).   If that lookup fails (because that
block has been reused), they then try and lookup the compressed
fragment block in the compressed block cache.  If that fails, the
data has to be read from the output filesystem.

The threads are "protected" from looking up a compressed fragment
before it is ready (i.e. still being compressed) because the
uncompressed fragment block by definition will always be found, because
it is released only once the compression has finished and the compressed
fragment block queued to the fragment orderer thread.

But the updating of the fragment table in the fragment orderer thread,
leaves a window where the uncompressed fragment block has been
released, but the fragment table hasn't been updated, which leads
to a failure in the fragment de-duplication threads using
uninitialised data.

Obviously, this will only happen if the uncompressed fragment
block has been (almost immediately) re-used within that window.
This will only occur with a very small fragment block cache of
a couple of blocks.

squashfs-tools/mksquashfs.c
@@ -2521,8 +2521,9 @@ static void *frag_order_deflator(void *arg)
 			file_buffer->size, block_size, noF, 1);
 		write_buffer->block = file_buffer->block;
 		write_buffer->sequence = file_buffer->sequence;
-		write_buffer->size = c_byte;
+		write_buffer->size = SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);
 		write_buffer->fragment = FALSE;
+		fragment_table[file_buffer->block].size = c_byte;
 		seq_queue_put(to_order, write_buffer);
 		TRACE("Writing fragment %lld, uncompressed size %d, "
 			"compressed size %d\n", file_buffer->block,
@@ -2541,11 +2542,9 @@ static void *frag_orderer(void *arg)
 		int block = write_buffer->block;
 
 		pthread_mutex_lock(&fragment_mutex);
-		fragment_table[block].size = write_buffer->size;
 		fragment_table[block].start_block = bytes;
 		write_buffer->block = bytes;
 		bytes += SQUASHFS_COMPRESSED_SIZE_BLOCK(write_buffer->size);
-		write_buffer->size = SQUASHFS_COMPRESSED_SIZE_BLOCK(write_buffer->size);
 		fragments_outstanding --;
 		log_fragment(block, write_buffer->block);
 		queue_put(to_writer, write_buffer);

commit 40eaa81b5fb9892d791f37e0766e481aba661103

Mksquashfs/Sqfstar: fix race condition in fragment table update
This commit fixes a regression introduced in commit 94db791f1
(August 2022).  The race condition is caused by the update
of the "fragment table" without the fragment mutex being held.

The race condition has been seen to occur with the -tailends option,
which is non-default for Mksquashfs, but, default for Sqfstar.
The effect of the race-condition is to produce a corrupted
fragment table.

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -2673,7 +2673,10 @@ static void *frag_order_deflator(void *arg)
 		write_buffer->sequence = file_buffer->sequence;
 		write_buffer->size = SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);
 		write_buffer->fragment = FALSE;
+		pthread_cleanup_push((void *) pthread_mutex_unlock, &fragment_mutex);
+		pthread_mutex_lock(&fragment_mutex);
 		fragment_table[file_buffer->block].size = c_byte;
+		pthread_cleanup_pop(1);
 		seq_queue_put(to_order, write_buffer);
 		TRACE("Writing fragment %lld, uncompressed size %d, "
 			"compressed size %d\n", file_buffer->block,