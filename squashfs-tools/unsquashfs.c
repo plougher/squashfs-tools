d8cb82d9840330f9344ec37b992595b5d7b44184
Phillip Lougher <phillip@squashfs.org.uk>
2023-03-25 20:50:59 +0000

    Squashfs tools 4.6.1 Release
    This is a bug fix update to the 4.6 release.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -3896,9 +3896,9 @@ static void print_cat_options(FILE *stream, char *name)
 	fprintf(stream, "-no-exit-code options for how they affect\nthe exit ");
 	fprintf(stream, "status.\n");
 	fprintf(stream, "\nSee also:\n");
-	fprintf(stream, "The README for the Squash-tools 4.6 release, ");
+	fprintf(stream, "The README for the Squashfs-tools 4.6.1 release, ");
 	fprintf(stream, "describing the new features can be\n");
-	fprintf(stream, "read here https://github.com/plougher/squashfs-tools/blob/master/README-4.6\n");
+	fprintf(stream, "read here https://github.com/plougher/squashfs-tools/blob/master/README-4.6.1\n");
 
 	fprintf(stream, "\nThe Squashfs-tools USAGE guide can be read here\n");
 	fprintf(stream, "https://github.com/plougher/squashfs-tools/blob/master/USAGE-4.6\n");
@@ -4040,9 +4040,9 @@ static void print_options(FILE *stream, char *name)
 	fprintf(stream, "-no-exit-code options for how they affect\nthe exit ");
 	fprintf(stream, "status.\n");
 	fprintf(stream, "\nSee also:\n");
-	fprintf(stream, "The README for the Squash-tools 4.6 release, ");
+	fprintf(stream, "The README for the Squashfs-tools 4.6.1 release, ");
 	fprintf(stream, "describing the new features can be\n");
-	fprintf(stream, "read here https://github.com/plougher/squashfs-tools/blob/master/README-4.6\n");
+	fprintf(stream, "read here https://github.com/plougher/squashfs-tools/blob/master/README-4.6.1\n");
 
 	fprintf(stream, "\nThe Squashfs-tools USAGE guide can be read here\n");
 	fprintf(stream, "https://github.com/plougher/squashfs-tools/blob/master/USAGE-4.6\n");
