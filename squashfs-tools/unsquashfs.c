0f7ec45f200c5963aad2d572e847f6e53cdebe47
Phillip Lougher <phillip@squashfs.org.uk>
2024-04-24 00:02:10 +0100

    unsquashfs: zero_data is only used in write_block()
    So move the variable into that function and make it a static.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -919,11 +919,11 @@ int write_bytes(int fd, char *buff, int bytes)
 }
 
 
-char *zero_data = NULL;
 
 static int write_block(int file_fd, char *buffer, int size, long long hole, int sparse)
 {
 	static int lseek_broken = FALSE;
+	static char *zero_data = NULL;
 	off_t off = hole;
 
 	if(hole) {
