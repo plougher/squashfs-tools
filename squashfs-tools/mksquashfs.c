plougher/squashfs-tools

commit ab480872bca64deb2e41df9eef16ed3b6ed3376f

mksquashfs: allow -Xhelp and -comp only to be specified
Previously you had to specify (a redundant) source and destination
to use the -Xhelp option, either on its own or in conjuction with -comp,
e.g. the following wasn't possible

% mksquashfs -comp xz -Xhelp
./mksquashfs: selected compressor "xz".  Options supported:
          -Xbcj filter1,filter2,...,filterN
                Compress using filter1,filter2,...,filterN in turn
                (in addition to no filter), and choose the best compression.
                Available filters: x86, arm, armthumb, powerpc, sparc, ia64
          -Xdict-size <dict-size>
                Use <dict-size> as the XZ dictionary size.  The dictionary size
                can be specified as a percentage of the block size, or as an
                absolute value.  The dictionary size must be less than or equal
                to the block size and 8192 bytes or larger.  It must also be
                storable in the xz header as either 2^n or as 2^n+2^(n+1).
                Example dict-sizes are 75%, 50%, 37.5%, 25%, or 32K, 16K, 8K
                etc.

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -7382,6 +7382,7 @@ int main(int argc, char *argv[])
 	int progress = TRUE;
 	int force_progress = FALSE;
 	int exclude_option = FALSE;
+	int Xhelp = FALSE;
 	struct file_buffer **fragment = NULL;
 	char *command;
 
@@ -7441,26 +7442,12 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	if(i < 3) {
-		ERROR("%s: fatal error: no arguments specified on command line\n\n", argv[0]);
-		print_help(TRUE, argv[0]);
-	}
-
-	option_offset = i;
-	destination_file = argv[i - 1];
-
-	if(argv[1][0] != '-') {
-		source_path = argv + 1;
-		source = i - 2;
-	} else {
-		source_path = NULL;
-		source = 0;
-	}
-
 	/*
 	 * Scan the command line for -comp xxx option, this is to ensure
 	 * any -X compressor specific options are passed to the
-	 * correct compressor
+	 * correct compressor.
+	 *
+	 * Also scan for -Xhelp specified on command line,
 	 */
 	for(; i < argc; i++) {
 		struct compressor *prev_comp = comp;
@@ -7488,7 +7475,9 @@ int main(int argc, char *argv[])
 			}
 			compressor_opt_parsed = 1;
 
-		} else if(strcmp(argv[i], "-e") == 0)
+		} else if(strcmp(argv[i], "-Xhelp") == 0)
+			Xhelp = TRUE;
+		else if(strcmp(argv[i], "-e") == 0)
 			break;
 		else if(option_with_arg(argv[i], option_table))
 			i++;
@@ -7501,6 +7490,27 @@ int main(int argc, char *argv[])
 	 */
 	if(comp == NULL)
 		comp = lookup_compressor(COMP_DEFAULT);
+
+	if(Xhelp) {
+		print_compressor_options(stdout, comp, argv[0]);
+		exit(0);
+	}
+
+	if(i < 3) {
+		ERROR("%s: fatal error: no arguments specified on command line\n\n", argv[0]);
+		print_help(TRUE, argv[0]);
+	}
+
+	option_offset = i;
+	destination_file = argv[i - 1];
+
+	if(argv[1][0] != '-') {
+		source_path = argv + 1;
+		source = i - 2;
+	} else {
+		source_path = NULL;
+		source = 0;
+	}
 
 	/*
 	 * Scan the command line for -cpiostyle, -tar and -pf xxx options, this

commit 39b8d8f2d7e13bea18db40ae3d52cd47a90b76e9

mksquashfs.c: fix parser regression
Fix parser regression caused by commit ab480872bca64deb2e41df9eef16ed3b6ed3376f

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -7371,7 +7371,7 @@ print_sqfstar_compressor_options:
 int main(int argc, char *argv[])
 {
 	struct stat buf, source_buf;
-	int res, i;
+	int res, i, j;
 	char *root_name = NULL;
 	squashfs_inode inode;
 	int readq;
@@ -7450,19 +7450,19 @@ int main(int argc, char *argv[])
 	 *
 	 * Also scan for -Xhelp specified on command line,
 	 */
-	for(; i < argc; i++) {
+	for(j = i; j < argc; j++) {
 		struct compressor *prev_comp = comp;
 		
-		if(strcmp(argv[i], "-comp") == 0) {
-			if(++i == argc) {
+		if(strcmp(argv[j], "-comp") == 0) {
+			if(++j == argc) {
 				ERROR("%s: -comp missing compression type\n",
 					argv[0]);
-				print_option_help(argv[0], argv[i - 1]);
+				print_option_help(argv[0], argv[j - 1]);
 			}
-			comp = lookup_compressor(argv[i]);
+			comp = lookup_compressor(argv[j]);
 			if(!comp->supported) {
 				ERROR("%s: Compressor \"%s\" is not supported!"
-					"\n", argv[0], argv[i]);
+					"\n", argv[0], argv[j]);
 				ERROR("%s: Compressors available:\n", argv[0]);
 				display_compressors(stderr, "", COMP_DEFAULT);
 				exit(1);
@@ -7476,20 +7476,20 @@ int main(int argc, char *argv[])
 			}
 			compressor_opt_parsed = 1;
 
-		} else if(strcmp(argv[i], "-help-comp") == 0) {
-			if(++i == argc) {
+		} else if(strcmp(argv[j], "-help-comp") == 0) {
+			if(++j == argc) {
 				ERROR("%s: -help-comp missing compressor name\n",
 					argv[0]);
-				print_option_help(argv[0], argv[i - 1]);
+				print_option_help(argv[0], argv[j - 1]);
 			}
 
-			help_comp = argv[i];
-		} else if(strcmp(argv[i], "-Xhelp") == 0)
+			help_comp = argv[j];
+		} else if(strcmp(argv[j], "-Xhelp") == 0)
 			Xhelp = TRUE;
-		else if(strcmp(argv[i], "-e") == 0)
+		else if(strcmp(argv[j], "-e") == 0)
 			break;
-		else if(option_with_arg(argv[i], option_table))
-			i++;
+		else if(option_with_arg(argv[j], option_table))
+			j++;
 	}
 
 	/*