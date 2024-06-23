03915040c6dd67cc9b6378236a4cf28120664c85
Phillip Lougher <phillip@squashfs.org.uk>
2024-06-23 22:16:54 +0100

    mksquashfs-manpage.sh: update to handle PAGER environment variable
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- generate-manpages/mksquashfs-manpage.sh
+++ generate-manpages/mksquashfs-manpage.sh
@@ -234,6 +234,23 @@ b again
 s/\([^.]\)\n/\1.\n/
 }" $tmp/mksquashfs.help
 
+# Concatenate the PAGER text on to one line.  Indent the line by
+# two and add a full stop to the end of the line
+
+${SED} -i " /PAGER/ {
+s/PAGER/  PAGER/
+
+:again
+N
+/\n$/b print
+s/\n */ /
+b again
+
+:print
+s/\([^.]\)\n/\1.\n/
+}" $tmp/mksquashfs.help
+
+# Make Compressors available header into a manpage section
 # Make Compressors available header into a manpage section
 
 ${SED} -i "s/\(Compressors available and compressor specific options\):/*\1*/" $tmp/mksquashfs.help
