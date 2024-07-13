17e5eb6e9502de6302cefa596dd8711c4fedaae1
Phillip Lougher <phillip@squashfs.org.uk>
2024-07-13 23:03:42 +0100

    unsquashfs: use unlink_file() for sockets
    When creating sockets, if -force specified, call unlink_file() to
    ensure any existing file is deleted using rmdir() or unlink()
    depending on whether the file exits and what type it is.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -1373,6 +1373,9 @@ static int create_inode(char *pathname, struct inode *i)
 		case SQUASHFS_LSOCKET_TYPE:
 			TRACE("create_inode: socket\n");
 
+			if(force)
+				unlink_file(pathname, NULL);
+
 			res = mknod(pathname, S_IFSOCK, 0);
 			if (res == -1) {
 				ERROR("create_inode: failed to create socket "
