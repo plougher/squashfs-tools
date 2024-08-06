8290ef4c5626a8f34ff5b66fe07b78f3e1e0ec9f
Phillip Lougher <phillip@squashfs.org.uk>
2024-08-06 22:36:49 +0100

    unsquashfs_help: add -help-all/-ha help text
    Also update the help text for -h[elp] option.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs_help.c
+++ squashfs-tools/unsquashfs_help.c
@@ -109,12 +109,15 @@ static char *unsquashfs_text[]={
 	"\t-st[rict-errors]\ttreat all errors as fatal\n",
 	"\t-no-exit[-code]\t\tdo not set exit code (to nonzero) on non-fatal errors\n",
 	"\n", "Help options:", "\n",
-	"\t-h[elp]\t\t\toutput this options text to stdout\n",
+	"\t-h[elp]\t\t\tprint help summary information to stdout\n",
 	"\t-help-option <regex>\tprint the help information for Unsquashfs "
 		"options matching <regex> to stdout\n",
 	"\t-help-section <section>\tprint the help information for section <section> to stdout.  Use \"sections\" or \"h\" as section name to get a list of sections and their names\n",
+	"\t-help-all\t\tprint help information for all Unsquashfs options and "
+		"sections to stdout\n",
 	"\t-ho <regex>\t\tshorthand alternative to -help-option\n",
 	"\t-hs <section>\t\tshorthand alternative to -help-section\n",
+	"\t-ha\t\t\tshorthand alternative to -help-all\n",
 	"\n", "Miscellaneous options:", "\n",
 	"\t-o[ffset] <bytes>\tskip <bytes> at start of FILESYSTEM.  Optionally a suffix of K, M or G can be given to specify Kbytes, Mbytes or Gbytes respectively (default 0 bytes).\n",
 	"\t-fstime\t\t\tsynonym for -mkfs-time\n",
