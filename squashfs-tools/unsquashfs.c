a3b45ae3cbfcc20ae4ca8687ae4e78b1f7cd55f6
Phillip Lougher <phillip@squashfs.org.uk>
2024-08-29 20:02:24 +0100

    sqfscat: Add option -help-all/-ha

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -3938,7 +3938,9 @@ static int parse_cat_options(int argc, char *argv[])
 		if(strcmp(argv[i], "-help") == 0 || strcmp(argv[i], "-h") == 0) {
 			sqfscat_help(FALSE, argv[0]);
 			exit(0);
-		} else if(strcmp(argv[i], "-help-option") == 0 || strcmp(argv[i], "-ho") == 0) {
+		} else if(strcmp(argv[i], "-help-all") == 0 || strcmp(argv[i], "-ha") == 0)
+			sqfscat_help_all(argv[0]);
+		else if(strcmp(argv[i], "-help-option") == 0 || strcmp(argv[i], "-ho") == 0) {
 			if(++i == argc) {
 				ERROR("%s: %s missing regex\n", argv[0], argv[i - 1]);
 				exit(1);
