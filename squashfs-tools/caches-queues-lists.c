ce42ca59c59917be26ad49671fec9c91658b73d2
Phillip Lougher <phillip@squashfs.org.uk>
2024-12-23 21:55:43 +0000

    caches-queues-lists: remove redundant code in queue_get_tid()
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/caches-queues-lists.c
+++ squashfs-tools/caches-queues-lists.c
@@ -141,9 +141,6 @@ void *queue_get_tid(int tid, struct queue *queue)
 			break;
 	}
 
-	while(queue->readp == queue->writep)
-		pthread_cond_wait(&queue->empty, &queue->mutex);
-
 	data = queue->data[queue->readp];
 	queue->readp = (queue->readp + 1) % queue->size;
 	pthread_cond_signal(&queue->full);
