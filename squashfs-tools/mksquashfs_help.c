2a2e1f3125686d14f0ad740a585121bf17b75db8
Phillip Lougher <phillip@squashfs.org.uk>
2025-01-27 22:57:36 +0000

    sqfstar_help: document new SQFS_CMDLINE environment variable
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/mksquashfs_help.c
+++ squashfs-tools/mksquashfs_help.c
@@ -693,6 +693,12 @@ static char *sqfstar_text[]={
 			"permission bits from the user, group or other of the "
 			"file respectively.\n",
 	"\n", "Environment:", "\n",
+	"SQFS_CMDLINE \t\tIf set, this is used as the directory to write the "
+		"file sqfs_cmdline which contains the command line arguments "
+		"given to Sqfstar.  Each command line argument is wrapped "
+	       "in quotes to ensure there is no ambiguity when arguments "
+	       "contain spaces.  If the file already exists the command "
+	       "line is appended to the file\n", "\n",
 	"SOURCE_DATE_EPOCH\tIf set, this is used as the filesystem creation "
 		"timestamp.  Also any file timestamps which are after "
 		"SOURCE_DATE_EPOCH will be clamped to SOURCE_DATE_EPOCH.  "
