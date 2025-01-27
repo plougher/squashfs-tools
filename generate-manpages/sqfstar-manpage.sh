524cdb8fb2fb530566316b2cc4d472c2ebda7e1d
Phillip Lougher <phillip@squashfs.org.uk>
2025-01-27 22:58:08 +0000

    sqfstar-manpage: handle new SQFS_CMDLINE environment variable
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- generate-manpages/sqfstar-manpage.sh
+++ generate-manpages/sqfstar-manpage.sh
@@ -252,6 +252,22 @@ b again
 s/\([^.]\)\n/\1.\n/
 }" $tmp/sqfstar.help
 
+# Concatenate the SQFS_CMDLINE text on to one line.  Indent the line by
+# two and add a full stop to the end of the line
+
+${SED} -i " /SQFS_CMDLINE/ {
+s/SQFS_CMDLINE/  SQFS_CMDLINE/
+
+:again
+N
+/\n$/b print
+s/\n */ /
+b again
+
+:print
+s/\([^.]\)\n/\1.\n/
+}" $tmp/sqfstar.help
+
 # Make Compressors available header into a manpage section
 
 ${SED} -i "s/\(Compressors available and compressor specific options\):/*\1*/" $tmp/sqfstar.help
