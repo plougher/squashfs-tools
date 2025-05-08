1b136c5bd8584602948da6908ec0c19c87e0177c
Phillip Lougher <phillip@squashfs.org.uk>
2025-05-08 17:05:00 +0100

    Move generate-manpages into squashfs-tools
    Manpage generation is part of squashfs-tools building and installation.
    
    The generate scripts do not need to be at the top-level.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/generate-manpages/install-manpages.sh
+++ squashfs-tools/generate-manpages/install-manpages.sh
@@ -3,16 +3,16 @@
 
 if [ $# -lt 3 ]; then
 	echo "$0: Insufficient arguments." >&2
-	echo "$0: <path to git-root/source-root> <path to install manpages> <use prebuilt manpages=y/n>" >&2
+	echo "$0: <path to git-root OR source-root> <path to install manpages> <use prebuilt manpages=y/n>" >&2
 	exit 1;
 fi
 
-if [ ! -f $1/generate-manpages/functions.sh ]; then
-	echo "$0: <arg1> doesn't seem to contain the path to the git-root/source-root" >&2
+if [ ! -f $1/squashfs-tools/generate-manpages/functions.sh ]; then
+	echo "$0: <arg1> doesn't seem to contain the path to the git-root OR source-root" >&2
 	exit 1
 fi
 
-. $1/generate-manpages/functions.sh
+. $1/squashfs-tools/generate-manpages/functions.sh
 
 if [ -z "$2" ]; then
 	error "$0: Install path for manpages empty.  Skipping manpage install"
@@ -27,7 +27,8 @@ for i in gzip; do
 		exit 1
 	fi
 done
-cd $1/generate-manpages
+
+cd $1/squashfs-tools/generate-manpages
 
 # We must have help2man to generate "custom" manpages for the
 # built squashfs-tools, incorporating build choices (the
@@ -36,20 +37,20 @@ cd $1/generate-manpages
 # Use the pre-built manpages if we've been told to use them ($3 = y), or
 # if help2man doesn't exist, or the manpage generation fails.
 
-source=../manpages
+source=../../manpages
 
 if [ $3 = "y" ]; then
 	print "$0: Using pre-built manpages"
 elif which help2man > /dev/null 2>&1; then
 	for i in mksquashfs unsquashfs sqfstar sqfscat; do
-		if ! ./$i-manpage.sh ../squashfs-tools ../squashfs-tools/$i.1; then
+		if ! ./$i-manpage.sh ../ ../$i.1; then
 			error "$0: Failed to generate manpage.  Falling back to using pre-built manpages"
 			failed="y"
 			break
 		fi
 	done
 
-	[ -z "$failed" ] && source=../squashfs-tools
+	[ -z "$failed" ] && source=../
 else
 	error "$0: ERROR - No help2man in PATH.  Cannot generate manpages."
 	failed="y"
