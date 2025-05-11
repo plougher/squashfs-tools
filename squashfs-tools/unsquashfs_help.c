e3c7eb3479a338fc588cbca485a262040ba748bd
Phillip Lougher <phillip@squashfs.org.uk>
2025-05-11 21:54:54 +0100

    unsquashfs_usage: Update "See also" section for 4.7 release
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs_help.c
+++ squashfs-tools/unsquashfs_help.c
@@ -244,11 +244,12 @@ static char *unsquashfs_text[]={
 	"\nSee -ignore-errors, -strict-errors and -no-exit-code options for "
 		"how they affect the exit status.\n",
 	"\n", "See also (extra information elsewhere):", "\n",
-	"The README for the Squashfs-tools 4.6.1 release, describing the new "
+	"The README for the Squashfs-tools 4.7 release, describing the new "
 		"features can be read here https://github.com/plougher/"
-			"squashfs-tools/blob/master/README-4.6.1\n",
-	"\nThe Squashfs-tools USAGE guide can be read here https://github.com/"
-		"plougher/squashfs-tools/blob/master/USAGE-4.6\n",
+		"squashfs-tools/blob/master/Documentation/4.7/README\n",
+	"\nThe Squashfs-tools USAGE guides and other documentation can be read "
+		"here https://github.com/plougher/squashfs-tools/blob/master/"
+		"Documentation/4.7\n",
 	"\n", "Decompressors available:", "\n",
 	"\t" DECOMPRESSORS "\n", NULL
 };
@@ -309,11 +310,12 @@ static char *sqfscat_text[]={
 	"\nSee -ignore-errors, -strict-errors and -no-exit-code options for "
 		"how they affect the exit status.\n",
 	"\n", "See also (extra information elsewhere):", "\n",
-	"The README for the Squashfs-tools 4.6.1 release describing the new "
+	"The README for the Squashfs-tools 4.7 release, describing the new "
 		"features can be read here https://github.com/plougher/"
-		"squashfs-tools/blob/master/README-4.6.1\n",
-	"\nThe Squashfs-tools USAGE guide can be read here https://github.com/"
-		"plougher/squashfs-tools/blob/master/USAGE-4.6\n",
+		"squashfs-tools/blob/master/Documentation/4.7/README\n",
+	"\nThe Squashfs-tools USAGE guides and other documentation can be read "
+		"here https://github.com/plougher/squashfs-tools/blob/master/"
+		"Documentation/4.7\n",
 	"\n", "Decompressors available:", "\n",
 	"\t" DECOMPRESSORS "\n", NULL,
 };
