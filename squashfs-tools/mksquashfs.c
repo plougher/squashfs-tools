plougher/squashfs-tools

commit 260c2429d24eec430af88215f53b27dabdc9889c

mksquashfs: compute latest inode time in dir_scan6()
Previously to implement -mkfs-time inode, the latest inode time
was computed in create_inode().

This has the drawback that the latest inode time is known *after*
dir_scan8() and create_inode() has been called for all inodes.   This
is OK for computing the mkfs-time, but, not if you want to know the
latest inode time *before* dir_scan8() and create_inode() being called.

So move the computation to dir_scan6().

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -1078,9 +1078,6 @@ static inline unsigned int get_time(time_t orig)
 			time = inode_time;
 	}
 
-	if(mkfs_time_inode && mkfs_time_latest < time)
-		mkfs_time_latest = time;
-
 	return time;
 }
 
@@ -3503,6 +3500,9 @@ squashfs_inode do_directory_scans(struct dir_ent *dir_ent, int progress)
 	 */
 	dir_scan6(root_dir);
 
+	if(mkfs_time_inode)
+		mkfs_time_latest = get_time(mkfs_time_latest);
+
 	alloc_inode_no(dir_ent->inode, root_inode_number);
 
 	/* Increase the progress bar by 5%, and map the inode count to
@@ -4342,6 +4342,9 @@ static void dir_scan6(struct dir_info *dir)
 
 		if(dir_ent->inode->root_entry)
 			continue;
+
+		if(mkfs_time_inode && mkfs_time_latest < dir_ent->inode->buf.st_mtime)
+			mkfs_time_latest = dir_ent->inode->buf.st_mtime;
 
 		alloc_inode_no(dir_ent->inode, 0);
 

commit a451e69a55d8745acde603219d2cc48224075637

mksquashfs: Fix "compute latest inode in dir_scan6()"
Commit 260c2429d24eec430af88215f53b27dabdc9889c introduced a regression
where the root inode timestamp is not evaluated against the
latest inode time.

For perhaps obvious reasons, this regression came to light
in testing the previous commit, where it was discovered the
root inode time didn't affect the latest inode time.

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -3540,6 +3540,9 @@ squashfs_inode do_directory_scans(struct dir_ent *dir_ent, int progress)
 	 */
 	dir_scan6(root_dir);
 
+	if((mkfs_inode_opt || root_inode_opt) && inode_time_latest < root_dir->dir_ent->inode->buf.st_mtime)
+		inode_time_latest = root_dir->dir_ent->inode->buf.st_mtime;
+
 	if(mkfs_inode_opt || root_inode_opt)
 		inode_time_latest = get_time(inode_time_latest);
 