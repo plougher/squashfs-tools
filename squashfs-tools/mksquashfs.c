plougher/squashfs-tools

commit 472588dc40292fa3d047369f1e758016a6b26e70

mksquashfs: handle completely sparse files
get_marked_vpos() returns the virtual disk write position when the
write_file function was entered.  This represents the virtual start
position of any data written.

However, if no data is written then there may exist no virtual position
to real disk position mapping, which will cause the virtual -> real disk
position lookup to fail.

The rare circumstance when this happens is where the multi-block file is
entirely sparse, which means no buffers are written for that file.  Thus
there is no virtual -> real disk position mapping created for that file.
Ordinarily, a later multi-block file will create a virtual -> real disk
position mapping when file blocks are written for that file.  But if the
entirely sparse file doesn't precede a multi-block file which provides
a mapping, then the virtual -> real disk position lookup will fail.

The file in this case should not be dependent on a later file
providing a mapping.  So this commit checks to see if no mapping
has been created, and asks the orderer thread to create one.

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -526,6 +526,20 @@ static inline void  sync_orderer_thread()
 }
 
 
+static inline void send_orderer_create_map(long long vpos)
+{
+	struct file_buffer *buffer = MALLOC(sizeof(struct file_buffer));
+
+	buffer->cache = NULL;
+	buffer->sequence = get_sequence();
+	buffer->buffer_type = MAP_CMD;
+	buffer->block = vpos;
+
+	order_queue_put(to_order, buffer);
+}
+
+
+
 static inline void put_write_buffer_hash(struct file_buffer *buffer)
 {
 	buffer->block = get_and_inc_vpos(buffer->size);
@@ -2757,6 +2771,9 @@ static void *orderer(void *arg)
 		} else if(write_buffer->buffer_type == RESET_CMD) {
 			set_dpos(get_virt_disk(write_buffer->block));
 			free(write_buffer);
+		} else if(write_buffer->buffer_type == MAP_CMD) {
+			add_virt_disk(block, get_dpos());
+			free(write_buffer);
 		} else
 
 			BAD_ERROR("Bug in orderer\n");
@@ -2880,6 +2897,9 @@ static struct file_info *write_file_process(int *status, struct dir_ent *dir_ent
 			fragment_buffer ?  fragment_buffer->checksum : 0, FALSE,
 			TRUE);
 
+	if(!is_vpos_marked())
+		send_orderer_create_map(get_marked_vpos());
+
 	gen_cache_block_put(fragment_buffer);
 	file_count ++;
 	total_bytes += read_size;
@@ -2973,6 +2993,9 @@ static struct file_info *write_file_blocks_dup(int *status, struct dir_ent *dir_
 		for(block = thresh; block < blocks; block ++)
 			if(buffer_list[block])
 				put_write_buffer_hash(buffer_list[block]);
+
+		if(!is_vpos_marked())
+			send_orderer_create_map(get_marked_vpos());
 	} else {
 		for(block = thresh; block < blocks; block ++)
 			gen_cache_block_put(buffer_list[block]);
@@ -3075,6 +3098,9 @@ static struct file_info *write_file_blocks(int *status, struct dir_ent *dir_ent,
 		file = create_non_dup(read_size, file_bytes, blocks, sparse,
 			block_list, get_marked_vpos(), fragment, 0, fragment_buffer ?
 			fragment_buffer->checksum : 0, FALSE, TRUE);
+
+	if(!is_vpos_marked())
+		send_orderer_create_map(get_marked_vpos());
 
 	gen_cache_block_put(fragment_buffer);
 	file_count ++;

squashfs-tools/virt_disk_pos.h
@@ -135,6 +135,15 @@ static inline void set_pos(long long value)
 }
 
 
+static inline int is_vpos_marked(void)
+{
+	if(marked_vpos == 0)
+		BAD_ERROR("BUG: Saved write position is empty!\n");
+	else
+		return marked_vpos != 1;
+}
+
+
 #define VIRT_DISK_HASH_SIZE 1048576
 #define VIRT_DISK_HASH(hash) (hash & 1048575)
 

squashfs-tools/caches-queues-lists.h
@@ -91,6 +91,7 @@ void remove_##NAME##_hash_table(TYPE *container, struct file_buffer *entry, int 
 #define WSYNC_CMD	3
 #define OSYNC_CMD	4
 #define RESET_CMD	5
+#define MAP_CMD		6
 
 /* struct describing a cache entry passed between threads */
 struct file_buffer {

commit 767d44ceff8fc8e1e7a47a0bcafdcc40eaeb6503

mksquashfs: don't create duplicate virtual -> real disk mappings
This bug is caused by commit 472588dc40292fa3d047369f1e758016a6b26e70,
but it is exposed by commit 078529915caab11e85299aac1304671151d507a3.

The first commit adds a virtual -> real disk mapping in cases where a
multi-block file contains all sparse blocks.   In this case no data
block has been written for this file, and it may mean no virtual -> real
disk mapping will exist otherwise.

However, a side effect of this is two duplicate virtual -> real disk
mappings may be created, with different values.

Now previously before commit 078529915caab11e85299aac1304671151d507a3,
Mksquashfs waited for all outstanding data blocks to be processed by the
orderer thread before creating the metadata.  At this point all the
duplicate virtual -> real disk mappings will exist, and the lookup
routine will return the last one created - which happens to be the
correct one.

After commit  078529915caab11e85299aac1304671151d507a3, Mksquashfs no
longer waits for all outstanding data blocks to be processed by the
orderer thread, which means when the lookup takes place in metadata
creation, there may exist only the first wrong duplicate, which will be
returned.  Obviously there may exist both duplicate values in which case
the correct one will be returned.   In other words this is a race
condition,  and it is highly dependent on hardware and input data
whether it triggers.

The fix is to increment the virtual disk position after creating a
mapping when a multi-block file contains all sparse blocks.  This
obviously prevents duplicate mappings from being created.

Fixes https://github.com/plougher/squashfs-tools/issues/339

Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

squashfs-tools/mksquashfs.c
@@ -2923,8 +2923,10 @@ static struct file_info *write_file_process(int *status, struct dir_ent *dir_ent
 			fragment_buffer ?  fragment_buffer->checksum : 0, FALSE,
 			TRUE);
 
-	if(!is_vpos_marked())
+	if(!is_vpos_marked()) {
 		send_orderer_create_map(get_marked_vpos());
+		inc_vpos();
+	}
 
 	gen_cache_block_put(fragment_buffer);
 	file_count ++;
@@ -3027,8 +3029,10 @@ static struct file_info *write_file_blocks_dup(int *status, struct dir_ent *dir_
 			if(buffer_list[block])
 				put_write_buffer_hash(buffer_list[block]);
 
-		if(!is_vpos_marked())
+		if(!is_vpos_marked()) {
 			send_orderer_create_map(get_marked_vpos());
+			inc_vpos();
+		}
 	} else {
 		for(block = thresh; block < blocks; block ++)
 			gen_cache_block_put(buffer_list[block]);
@@ -3140,8 +3144,10 @@ static struct file_info *write_file_blocks(int *status, struct dir_ent *dir_ent,
 			block_list, get_marked_vpos(), fragment, 0, fragment_buffer ?
 			fragment_buffer->checksum : 0, FALSE, TRUE);
 
-	if(!is_vpos_marked())
+	if(!is_vpos_marked()) {
 		send_orderer_create_map(get_marked_vpos());
+		inc_vpos();
+	}
 
 	gen_cache_block_put(fragment_buffer);
 	file_count ++;

squashfs-tools/virt_disk_pos.h
@@ -96,6 +96,15 @@ static inline long long get_and_inc_vpos(long long value)
 }
 
 
+static inline void inc_vpos()
+{
+	if(marked_vpos == 0)
+		BAD_ERROR("BUG: Saved write position is empty!\n");
+
+	vpos ++;
+}
+
+
 static inline int reset_vpos(void)
 {
 	if(marked_vpos == 0)