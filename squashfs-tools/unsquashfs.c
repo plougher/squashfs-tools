40067c94b067915dc8aa473ac657fb946a05f86a
Phillip Lougher <phillip@squashfs.org.uk>
2026-06-07 23:28:30 +0100

    unsquashfs: -follow-symlink option will now follow leaf symbolic links
    -follow-symlink semantics have been changed to mean follow leaf symbolic
    links, because everything else (intermediate symbolic links, ".." and
    ".") is now followed by default.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/unsquashfs.c
+++ squashfs-tools/unsquashfs.c
@@ -88,6 +88,7 @@ int ignore_errors = FALSE;
 int strict_errors = FALSE;
 int use_localtime = TRUE;
 int max_depth = -1; /* unlimited */
+int leaf_symlink = FALSE;
 int missing_paths = FALSE;
 int no_wildcards = FALSE;
 int set_exit_code = TRUE;
@@ -2397,7 +2398,7 @@ static int follow_extract_paths(char *path, char *newpath, int symlinks,
 				 * leaf and so extract the symbolic link rather
 				 * than what it points to
 				 */
-				if(path[0] == '\0') {
+				if(path[0] == '\0' && !leaf_symlink) {
 					add_to_extracts(stack, name);
 					traversed = TRUE;
 					break;
@@ -4816,10 +4817,11 @@ static int parse_options(int argc, char *argv[])
 		} else if(strcmp(argv[i], "-no-exit-code") == 0 ||
 				strcmp(argv[i], "-no-exit") == 0)
 			set_exit_code = FALSE;
-		else if(strcmp(argv[i], "-follow-paths") == 0 ||
+		else if(strcmp(argv[i], "-follow-symlink") == 0 ||
 				strcmp(argv[i], "-follow") == 0 ||
 				strcmp(argv[i], "-L") == 0 ||
 				strcmp(argv[i], "-follow-symlinks") == 0) {
+			leaf_symlink = TRUE;
 			; // now default, option retained for backwards compatibility
 		} else if(strcmp(argv[i], "missing-paths") == 0 ||
 				strcmp(argv[i], "-missing") == 0 ||
