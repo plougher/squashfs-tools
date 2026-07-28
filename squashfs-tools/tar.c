52992df017ea9f80e37012947538249547a0d048
Phillip Lougher <phillip@squashfs.org.uk>
2026-07-28 17:28:47 +0100

    tar: fix deref of NULL pointer
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- squashfs-tools/tar.c
+++ squashfs-tools/tar.c
@@ -1421,7 +1421,7 @@ long long read_tar_file()
 		file_buffer->version = 0;
 		file_buffer->error = FALSE;
 		file_buffer->next_state = NEXT_FILE;
-		isreg = S_ISREG(tar_file->buf.st_mode);
+		isreg = tar_file ? S_ISREG(tar_file->buf.st_mode) : FALSE;
 		main_queue_put(to_main, file_buffer);
 
 		if(status == TAR_EOF)
